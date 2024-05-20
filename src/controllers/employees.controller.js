99e4674ade06e0d7f68926d0826fdda3ef1dd737
eddyperea05 <eddyperea05@gmail.com>
2024-05-20 16:29:57 -0500

    Se arreglo getEmployees

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,30 +1,31 @@
 import { pool } from "../db.js";
 
 export const getEmployees = async (req, res) => {
-
-    //res.send("Obteniendo empleados")
     //console.log("Obteniendo empleado")
 
-    try {
+    //res.send("Obteniendo empleados")
+
+    /*try {
         const employees = await Employee.find();
         res.json(employees);
     } catch (error) {
         res.status(500).send(error.message);
-    }
+    }*/
 
-    //const result = await pool.query("Select * from employee");
-    //res.send(result.json)
+    const [rows] = await pool.query("Select * from employee");
+    res.json(rows)
 
 };
 
 export const createEmployees = async (req, res) => {
-    res.send("POST SUCCESS")
-    /*const { name, salary } = req.body;
-    await pool.query("INSERT INTO employees (name, salary) VALUES (?,?)", [name, salary])
+    const { name, salary } = req.body;
+    await pool.query("INSERT INTO employee (name, salary) VALUES (?,?)", [name, salary])
     res.send(
         name,
         salary
-    )*/
+    )
+    //res.send("POST SUCCESS")
+
 }
 
 export const updateEmployees = (req, res) => res.send("Actualizando empleados");
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import express from "express"
 
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
-
-
+//import body-parse
+const port = 3000;
 const app = express()
 
 
-app.use(express.json);
+app.use(express.json());
 
+//Rourtes
 app.use(indexRoutes);
 app.use(employeesRoutes);
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
